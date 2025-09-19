cc386df6fbb7f96a6cc8f7b4d01a14f6d328d68e
Yotzovv <ivaylo.yotsov1@gmail.com>
2025-09-19 15:28:58 +0300

    service registration in Program.cs

--- eshop-microservices/Services/Basket/Basket.API/GlobalUsing.cs
+++ eshop-microservices/Services/Basket/Basket.API/GlobalUsing.cs
@@ -4,3 +4,6 @@ global using Carter;
 global using FluentValidation;
 global using Mapster;
 global using MediatR;
+global using BuildingBlocks.Behaviors;
+global using Catalog.API.BuildingBlocks.ValidationBehavior;
+
