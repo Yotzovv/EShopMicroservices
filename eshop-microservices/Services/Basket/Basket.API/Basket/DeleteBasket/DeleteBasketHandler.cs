55909de387ca2b4d6ccd022cf52e83ee457a7299
Yotzovv <ivaylo.yotsov1@gmail.com>
2025-09-19 15:22:45 +0300

    Add DeleteBasket endpoint and command handler for removing user shopping carts

---
+++ eshop-microservices/Services/Basket/Basket.API/Basket/DeleteBasket/DeleteBasketHandler.cs
@@ -0,0 +1,24 @@
+
+namespace Basket.API.Basket.DeleteBasket;
+
+public record DeleteBasketCommand(string UserName) : ICommand<DeleteBasketResult>;
+public record DeleteBasketResult(bool Success);
+
+public class DeleteBasketCommandValidator : AbstractValidator<DeleteBasketCommand>
+{
+    public DeleteBasketCommandValidator()
+    {
+        RuleFor(x => x.UserName).NotEmpty().WithMessage("UserName is required.");
+    }
+}
+
+public class DeleteBasketCommandHandler
+    : IRequestHandler<DeleteBasketCommand, DeleteBasketResult>
+{
+    public Task<DeleteBasketResult> Handle(DeleteBasketCommand command , CancellationToken cancellationToken)
+    {
+        // TODO: delete basket logic
+
+        return new DeleteBasketResult(true);
+    }
+}
