bbd1ea544420cf2b54d9489f35e1947ca7703197
Yotzovv <ivaylo.yotsov1@gmail.com>
2025-09-22 09:41:38 +0300

    add health checks for PostgreSQL and Redis

--- eshop-microservices/Services/Basket/Basket.API/Program.cs
+++ eshop-microservices/Services/Basket/Basket.API/Program.cs
@@ -1,5 +1,6 @@
 using BuildingBlocks.Exceptions.Handler;
-using Microsoft.Extensions.Caching.Distributed;
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -30,11 +31,19 @@ builder.Services.AddStackExchangeRedisCache(options =>
 });
 
 builder.Services.AddExceptionHandler<CustomExceptionHandler>();
+builder.Services.AddHealthChecks()
+    .AddNpgSql(builder.Configuration.GetConnectionString("Database")!)
+    .AddRedis(builder.Configuration.GetConnectionString("Redis")!);
 
 var app = builder.Build();
-
 // Configure the HTTP request pipeline
 app.MapCarter();
 app.UseExceptionHandler(options => { });
+
+app.MapHealthChecks("/health",
+    new HealthCheckOptions
+    {
+        ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+    });
 
 app.Run();
