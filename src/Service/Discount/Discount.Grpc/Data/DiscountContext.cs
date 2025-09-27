971783ecf903bf0b7ce1343c9fdcaf34d37db7ab
Yotzovv <ivaylo.yotsov1@gmail.com>
2025-09-27 17:31:06 +0300

    Add DiscountContext and configure database connection for gRPC service

---
+++ src/Service/Discount/Discount.Grpc/Data/DiscountContext.cs
@@ -0,0 +1,16 @@
+﻿using Discount.Grpc.Protos;
+using Microsoft.EntityFrameworkCore;
+
+namespace Discount.Grpc.Data
+{
+    public class DiscountContext : DbContext
+    {
+        public DbSet<CouponModel> Coupons { get; set; } = default!;
+
+        public DiscountContext(DbContextOptions<DiscountContext> options)
+            : base(options)
+        {
+                
+        }
+    }
+}
