309298eb480aef31c08f721894f4c56c3fe9501f
Yotzovv <ivaylo.yotsov1@gmail.com>
2025-09-27 16:51:24 +0300

    Add DiscountService implementation for gRPC

---
+++ src/Service/Discount/Discount.Grpc/Services/DiscountService.cs
@@ -0,0 +1,22 @@
+using Discount.Grpc.Protos;
+using Grpc.Core;
+
+namespace Discount.Grpc.Services;
+
+public sealed class DiscountService : DiscountProtoService.DiscountProtoServiceBase
+{
+    public override Task<CouponModel> GetDiscount(GetDiscountRequest request, ServerCallContext context)
+    {
+        return base.GetDiscount(request, context);
+    }
+
+    public override Task<CouponModel> CreateDiscount(CreateDiscountRequest request, ServerCallContext context)
+    {
+        return base.CreateDiscount(request, context);
+    }
+
+    public override Task<DeleteDiscountResponse> DeleteDiscount(DeleteDiscountRequest request, ServerCallContext context)
+    {
+        return base.DeleteDiscount(request, context);
+    }
+}
