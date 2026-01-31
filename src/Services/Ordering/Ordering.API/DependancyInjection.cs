6cb65c0174ca6598c2deefb69ecd7832e9e65554
Yotzovv <ivaylo.yotsov1@gmail.com>
2026-01-31 18:02:13 +0200

    add dependancy injections

---
+++ src/Services/Ordering/Ordering.API/DependancyInjection.cs
@@ -0,0 +1,19 @@
+﻿namespace Ordering.API
+{
+    public static class DependancyInjection
+    {
+        public static IServiceCollection AddApiServices(this IServiceCollection services)
+        {
+            // Add Carter.
+            
+            // Add application services here
+            return services;
+        }
+
+        public static WebApplication UseApiServices(this WebApplication app)
+        {
+            // Configure the HTTP request pipeline.
+            return app;
+        }
+    }
+}
