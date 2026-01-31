8f80a840eea62a60b004dafbd070e1c35b241798
Yotzovv <ivaylo.yotsov1@gmail.com>
2026-01-31 18:02:46 +0200

    Section 13: Ordering Service (#4)
    * Initialize Ordering Service
    
    * Initialize Ordering.Application , Domain and Infrastructure projects
    
    * bring back file discount.proto
    
    * Add project references
    
    * add dependancy injections

---
+++ src/Services/Ordering/Ordering.Infrastructure/DependancyInjection.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+
+namespace Ordering.Infrastructure
+{
+    public static class DependancyInjection
+    {
+        public static IServiceCollection AddInfrastructureServices(
+            this IServiceCollection services, 
+            IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString("Database");
+
+            // Add services to the container.
+            
+            // Add application services here
+            return services;
+        }
+    }
+}
