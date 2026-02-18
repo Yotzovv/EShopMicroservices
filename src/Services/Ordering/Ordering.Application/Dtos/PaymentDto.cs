ddb23bd8dbeea8e728a3057450eaff734c88f5fe
Yotzovv <ivaylo.yotsov1@gmail.com>
2026-02-18 09:11:31 +0200

    develop Dtos

---
+++ src/Services/Ordering/Ordering.Application/Dtos/PaymentDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Ordering.Application.Dtos
+{
+    public record PaymentDto(
+        string CardName,
+        string CardNumber,
+        DateTime ExpirationDate,
+        string Cvv,
+        int PaymentMethod
+    );
+}
