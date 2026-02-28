9983a483b57130661e6a658903ef072805b46dff
Yotzovv <ivaylo.yotsov1@gmail.com>
2026-02-28 14:53:39 +0200

    develop OrderExtensions

---
+++ src/Services/Ordering/Ordering.Application/Orders/Query/GetOrdersByName/GetOrdersByNameQuery.cs
@@ -0,0 +1,6 @@
+﻿namespace Ordering.Application.Orders.Query.GetOrdersByName;
+
+public record GetOrdersByNameQuery(string Name)
+    : IQuery<GetOrdersByNameResult>;
+
+public record GetOrdersByNameResult(IEnumerable<OrderDto> Orders);
