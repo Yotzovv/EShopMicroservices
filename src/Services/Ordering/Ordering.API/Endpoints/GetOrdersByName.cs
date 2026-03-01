d3fac5b5015f62db7dd90b489a5488866bdfd513
Yotzovv <ivaylo.yotsov1@gmail.com>
2026-03-01 14:45:29 +0200

    develop endpoint GetOrdersByName

--- src/Services/Ordering/Ordering.API/Endpoints/GetOrdersByName.cs
+++ src/Services/Ordering/Ordering.API/Endpoints/GetOrdersByName.cs
@@ -1,6 +1,26 @@
-﻿namespace Ordering.API.Endpoints
+﻿
+using Ordering.Application.Orders.Query.GetOrdersByName;
+
+namespace Ordering.API.Endpoints
 {
-    public class GetOrdersByName
+    public record GetOrdersByNameResponse(IEnumerable<OrderDto> Orders);
+
+    public class GetOrdersByName : ICarterModule
     {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapGet("/orders/{userName}", async (string userName, ISender sender) =>
+            {
+                var query = new GetOrdersByNameQuery(userName);
+                var orders = await sender.Send(query);
+                var response = new GetOrdersByNameResponse(orders.Adapt<IEnumerable<OrderDto>>());
+                return Results.Ok(response);
+            })
+            .WithName("GetOrdersByName")
+            .Produces<GetOrdersByNameResponse>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status401Unauthorized)
+            .WithSummary("Get orders by user name")
+            .WithDescription("Get orders by user name");
+        }
     }
 }
