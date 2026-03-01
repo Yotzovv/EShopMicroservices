219d103cf5876eb2e99f671d2817c7c4e5524b78
Yotzovv <ivaylo.yotsov1@gmail.com>
2026-03-01 14:19:00 +0200

    develop endpoint CreateOrder

--- src/Services/Ordering/Ordering.API/Endpoints/CreateOrder.cs
+++ src/Services/Ordering/Ordering.API/Endpoints/CreateOrder.cs
@@ -1,6 +1,29 @@
-﻿namespace Ordering.API.Endpoints
+﻿using Ordering.Application.Orders.Commands.CreateOrder;
+
+namespace Ordering.API.Endpoints
 {
-    public class CreateOrder
+    public record CreateOrderRequest(OrderDto Order);
+    public record CreateOrderResponse(Guid Id);
+
+    public class CreateOrder : ICarterModule
     {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapPost("/orders", async (CreateOrderRequest request, ISender sender) =>
+            {
+                var command = request.Adapt<CreateOrderCommand>();
+
+                var result = await sender.Send(command);
+
+                var response = result.Adapt<CreateOrderResponse>();
+
+                return Results.Created($"/orders/{response.Id}", response);
+            })
+            .WithName("CreateOrder")
+            .Produces<CreateOrderResponse>(StatusCodes.Status201Created)
+            .ProducesProblem(StatusCodes.Status401Unauthorized)
+            .WithSummary("Create order")
+            .WithDescription("Create Order");
+        }
     }
 }
