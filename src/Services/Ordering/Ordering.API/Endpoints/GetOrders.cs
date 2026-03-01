5fa0ceaa7769311f3ab6bfce1db7c0714d48bc33
Yotzovv <ivaylo.yotsov1@gmail.com>
2026-03-01 15:07:22 +0200

    develop endpoint GetOrders

--- src/Services/Ordering/Ordering.API/Endpoints/GetOrders.cs
+++ src/Services/Ordering/Ordering.API/Endpoints/GetOrders.cs
@@ -1,6 +1,25 @@
-﻿namespace Ordering.API.Endpoints
+﻿using BuildingBlocks.Pagination;
+using Ordering.Application.Orders.Query.GetOrders;
+
+namespace Ordering.API.Endpoints
 {
-    public class GetOrders
+    public record GetOrdersResponse(PaginationResult<OrderDto> Orders);
+
+    public class GetOrders : ICarterModule
     {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapGet("/orders", async ([AsParameters] PaginationRequest request, ISender sender) =>
+            {
+                var result = await sender.Send(new GetOrdersQuery(request));
+                var response = result.Adapt<GetOrdersResponse>();
+                return Results.Ok(response);
+            })
+            .WithName("GetOrders")
+            .Produces<GetOrdersResponse>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status401Unauthorized)
+            .WithSummary("Get orders")
+            .WithDescription("Get orders");
+        }
     }
 }
