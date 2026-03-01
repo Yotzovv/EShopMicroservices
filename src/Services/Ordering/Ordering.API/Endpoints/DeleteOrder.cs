fe3c490d77a1020868ec7415c6829ef88085c5f6
Yotzovv <ivaylo.yotsov1@gmail.com>
2026-03-01 14:41:16 +0200

    develop endpoint DeleteOrder

--- src/Services/Ordering/Ordering.API/Endpoints/DeleteOrder.cs
+++ src/Services/Ordering/Ordering.API/Endpoints/DeleteOrder.cs
@@ -1,6 +1,26 @@
-﻿namespace Ordering.API.Endpoints
+﻿
+using Ordering.Application.Orders.Commands.DeleteOrder;
+
+namespace Ordering.API.Endpoints
 {
-    public class DeleteOrder
+    public record DeleteOrderResponse(bool isSuccess);
+
+    public class DeleteOrder : ICarterModule
     {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapDelete("/orders/{id:guid}", async (Guid id, ISender sender) =>
+            {
+                var command = new DeleteOrderCommand(id);
+                var response = await sender.Send(command);
+                var result = response.Adapt<DeleteOrderResponse>();
+                return Results.Ok(result);
+            })
+            .WithName("DeleteOrder")
+            .Produces<DeleteOrderResponse>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status401Unauthorized)
+            .WithSummary("Delete order")
+            .WithDescription("Delete Order");
+        }
     }
 }
