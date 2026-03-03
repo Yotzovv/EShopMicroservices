41119e42f7252e0803ca2acf8a88bed7e1a75722
Yotzovv <ivaylo.yotsov1@gmail.com>
2026-03-03 15:30:05 +0200

    add healthcheck SQLServer

--- src/Services/Ordering/Ordering.API/DependancyInjection.cs
+++ src/Services/Ordering/Ordering.API/DependancyInjection.cs
@@ -1,15 +1,19 @@
 ﻿using BuildingBlocks.Exceptions.Handler;
 using Carter;
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 
 namespace Ordering.API
 {
     public static class DependancyInjection
     {
-        public static IServiceCollection AddApiServices(this IServiceCollection services)
+        public static IServiceCollection AddApiServices(this IServiceCollection services, IConfiguration configuration)
         {
             services.AddCarter();
 
             services.AddExceptionHandler<CustomExceptionHandler>();
+            services.AddHealthChecks()
+                .AddSqlServer(configuration.GetConnectionString("Database")!);
 
             // Add application services here
             return services;
@@ -20,6 +24,11 @@ namespace Ordering.API
             app.MapCarter();
 
             app.UseExceptionHandler(options => { });
+            app.UseHealthChecks("/health",
+                new HealthCheckOptions
+                {
+                    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+                });
 
             return app;
         }
