1dcd0626d52a8ed649e9b03daf63e73be0f96b99
Yotzovv <ivaylo.yotsov1@gmail.com>
2026-03-06 14:27:19 +0200

    Add MassTransit RabbitMQ setup and config placeholders

---
+++ src/BuildingBlocks/BuildingBlocks/BuildingBlocks.Messaging/MasstTransit/Extensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace BuildingBlocks.Messaging.MasstTransit
+{
+    public static class Extensions
+    {
+        public static IServiceCollection AddMasstTransit(this IServiceCollection services, Assembly? assembly = null)
+        {
+            return services;
+        }
+    }
+}
